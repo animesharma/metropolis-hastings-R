02807a527b4e8e50ce239e3c9fff3631d0c5f1fc
Animesh <animesharma08+github@gmail.com>
2021-12-08 23:32:03 -0500

    Update means and covariance matrix

--- metropolis-hastings.R
+++ metropolis-hastings.R
@@ -1,18 +1,19 @@
 library("mvtnorm")
+
 dimensions = 4
-num_samples = 5000
-burn_in_threshold = 25
+num_samples = 1000
+burn_in_threshold = 500
 
 # Create a multivariate normal distribution f with some parameters
 f_means = runif(dimensions)
-f_cov = rWishart(1, dimensions, diag(dimensions))[, , 1]
-f = function(x) {
-	return(dmvnorm(x, f_means, f_cov))
+f_cov = diag(dimensions)
+f = function (x) {
+	return (dmvnorm(x, f_means, f_cov))
 }
 
 # Create a multivariate normal proposal distribution
 means = runif(dimensions)
-covariance_matrix =rWishart(1, dimensions, diag(dimensions))[, , 1]
+covariance_matrix = diag(dimensions)
 # Initialize previous with a random value
 previous = rmvnorm(1, mean = means, sigma = covariance_matrix)
 # Create an empty data frame to store the accepted values in
@@ -24,19 +25,18 @@ for(i in 1 : num_samples + burn_in_threshold){
 	while(TRUE) {
 		# Generate a random value from proposal distribution
 		current = rmvnorm(1, mean = means, sigma = covariance_matrix)
-		# Calculate alpha using the current and previous
+		# Calculate alpha using the current and previous values
 		numerator = f(current) * dmvnorm(previous, means, covariance_matrix) 
 		denominator = f(previous) * dmvnorm(current, means, covariance_matrix) 
 		alpha = numerator / denominator
 		# Check if the current value is feasible
 		if (runif(1) < min(alpha, 1)) {
-			# Append the current value in data frame once the burn-in threshold has been surpassed
-		  df = rbind(df, current)
-			if(i > 29) {
-			  print(i)
-			  #print(df)
-				covariance_matrix=cov(df)
-				means=colMeans(df)
+			# Append the current value in data frame
+			df = rbind(df, current)
+			# Update the means and covariance matrix once we have sufficient accepted values
+			if(i > burn_in_threshold + dimensions) {
+				covariance_matrix = cov(df)
+				means = colMeans(df)
 			}
 			# Update the value of previous to current if current is feasible
 			previous = current
@@ -45,6 +45,9 @@ for(i in 1 : num_samples + burn_in_threshold){
 		}
 	}
 }
+
+# Remove burn-in values from the list of accepted values
+df = df[-c(1 : burn_in_threshold), ]
 
 # Print the accepted values
 print(head(df, 25))
@@ -58,7 +61,6 @@ plot(
 print(colMeans(df))
 # Original means used for f
 print(f_means)
-
 # Estimated covariance matrix of accepted samples
 print(cov(df))
 # Original covariance matrix used for f
